0a969faa99a81e46106b5b1d9c4bca648c911289
abel <abeltimm@protonmail.com>
2022-10-05 11:32:43 +0200

    Update arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -1,3 +1,20 @@
 // https://github.com/Seeed-Studio/Grove_I2C_Motor_Driver_v1_3/archive/master.zip
 
 
+#include <Grove_I2C_Motor_Driver.h>
+
+// default I2C address is 0x0f
+#define I2C_ADDRESS 0x0f
+ 
+void setup()
+{
+    Motor.begin(I2C_ADDRESS);
+}
+
+// Set the speed of a motor, speed is equal to duty cycle here
+void speed(unsigned char motor_id, int _speed);
+
+delay(1000);
+
+// Stop one motor
+void stop(unsigned char motor_id);
