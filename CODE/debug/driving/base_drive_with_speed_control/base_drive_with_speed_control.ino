d24a3704500928f46999effa645d5e7a79454696
Abel Tim <abel@abel-ms7c95.home>
2025-03-20 21:59:14 +0100

    PWM with motors

--- CODE/debug/driving/base_drive_with_speed_control/base_drive_with_speed_control.ino
+++ CODE/debug/driving/base_drive_with_speed_control/base_drive_with_speed_control.ino
@@ -7,22 +7,6 @@ volatile int PWMval1 = 0;   // PWM value 0-100
 volatile int PWMval2 = 0;   // PWM value 0-100
 volatile int PWMval3 = 0;   // PWM value 0-100
 
-int incomingbyte1 = 0;  //the byte that it is reciefing
-bool iswriting1 = false;  //true if writing else fals
-String tlocdat1 = "";  //temporary location data
-String locdat1 = "";   //location data
-int nir1 = "-99"; // which ir sensor it is
-int pir1 = "-99"; // amount of pulses it had
-
-int incomingbyte2 = 0;  //same but for seeed xiao N 2
-bool iswriting2 = false;
-String tlocdat2 = ""; 
-String locdat2 = ""; 
-int nir2 = "-99";
-int pir2 = "-99";
-
-float degwhere = -99.0; // where the ball is in degrees
-
 int led = 13;
 
 int m1enA = 6;
@@ -45,27 +29,7 @@ int m3pwm1 = 24;
 int m3pwm2 = 25;
 int m3encoA = 28;
 int m3encoB = 23;
-
-int softpwr = 2;
-
-int pr1 = 13;
-int pr2 = 41;
-int pr3 = 40;
-int pr4 = 39;
-int pr5 = 38;
-int pr6 = 37;
-int pr7 = 36;
-int pr8 = 35;
-
-int dpr1 = "LOW";
-int dpr2 = "LOW";
-int dpr3 = "LOW";
-int dpr4 = "LOW";
-int dpr5 = "LOW";
-int dpr6 = "LOW";
-int dpr7 = "LOW";
-int dpr8 = "LOW";
-
+  
 void setup() {
   pinMode(m1enA, OUTPUT);
   pinMode(m1enB, OUTPUT);
@@ -81,26 +45,14 @@ void setup() {
   pinMode(m3enB, OUTPUT);
   pinMode(m3pwm1, OUTPUT);
   pinMode(m3pwm2, OUTPUT);
-
-  pinMode(softpwr, INPUT_PULLUP);
-
-  pinMode(pr1, INPUT);
-  pinMode(pr2, INPUT);
-  pinMode(pr3, INPUT);
-  pinMode(pr4, INPUT);
-  pinMode(pr5, INPUT);
-  pinMode(pr6, INPUT);
-  pinMode(pr7, INPUT);
-  pinMode(pr8, INPUT);
-
   Serial.begin(9600);
-  Serial1.begin(9600);
-  Serial2.begin(9600);
+  Serial.print("start");
 
   Timer3.initialize(800); // 800 microseconds / 1.25kHz
   Timer3.attachInterrupt(updatePWM1);
   Timer3.attachInterrupt(updatePWM2);
   Timer3.attachInterrupt(updatePWM3);
+  
 }
 
 void updatePWM1() {
@@ -172,7 +124,6 @@ void mcpsoftpwm3(int value3) {
   Serial.println(value3); 
   PWMval3 = value3; 
 }
-
 void drive(int speed, int direction){
  //
  // Direction............|.Motor 1.|.Motor 2.|.Motor 3.
@@ -258,112 +209,13 @@ void drive(int speed, int direction){
   }
 }
 
+// the loop routine runs over and over again forever:
 void loop() {
-  int sprst = digitalRead(softpwr);  //softpower status
-  if(sprst == LOW){
-    dpr1 = digitalRead(pr1);
-    dpr2 = digitalRead(pr2); 
-    dpr3 = digitalRead(pr3); 
-    dpr4 = digitalRead(pr4); 
-    dpr5 = digitalRead(pr5); 
-    dpr6 = digitalRead(pr6); 
-    dpr7 = digitalRead(pr7); 
-    dpr8 = digitalRead(pr8); 
-  
-    if(dpr1 == HIGH){
-      //go back to ?deg
-    }else if (dpr2 == HIGH){
-      //go back to ?deg
-    }else if (dpr3 == HIGH){
-      //go back to ?deg
-    }else if (dpr4 == HIGH){
-      //go back to ?deg
-    }else if (dpr5 == HIGH){
-      //go back to ?deg
-    }else if (dpr6 == HIGH){
-      //go back to ?deg
-    }else if (dpr7 == HIGH){
-      //go back to ?deg
-    }else if (dpr8 == HIGH){
-      //go back to ?deg
-    }else{
-      if (Serial1.available() > 0) {
-        incomingbyte1 = Serial1.read();
-        //Serial.print(char(incomingbyte1));
-        if (incomingbyte1 == 's'){
-          iswriting1 = true;
-          tlocdat1 = "";
-        }else if(incomingbyte1 == 'e'){
-          iswriting1 = false;
-          locdat1 = tlocdat1;        
-        }else if(iswriting1 == true){
-          //Serial.print(char(incomingbyte1));
-          tlocdat1 += (char)incomingbyte1;  
-        }
-      }
-      if (Serial2.available() > 0) {
-        incomingbyte2 = Serial2.read();
-        //Serial.print(char(incomingbyte2));
-        if (incomingbyte2 == 's'){
-          iswriting2 = true;
-          tlocdat2 = "";
-        }else if(incomingbyte1 == 'e'){
-          iswriting2 = false;
-          locdat2 = tlocdat1;        
-        }else if(iswriting2 == true){
-          //Serial.print(char(incomingbyte1));
-          tlocdat2 += (char)incomingbyte1;  
-        }
-      }
-      //Serial.println(locdat);
-      sscanf(locdat1.c_str(), "%d, %d", &nir1, &pir1);
-      sscanf(locdat2.c_str(), "%d, %d", &nir2, &pir2);
-      
-      
-      if(pir1 > pir2){
-         if(nir1 = 1){    //not good looking code because posbile wrong wireing
-          degwhere = 0;      
-         }else if(nir1 = 2){
-          degwhere = 22.5;
-         }else if(nir1 = 3){
-          degwhere = 45;
-         }else if(nir1 = 4){
-          degwhere = 67.5;
-         }else if(nir1 = 5){
-          degwhere = 90;
-         }else if(nir1 = 6){
-          degwhere = 112.5;
-         }else if(nir1 = 7){
-          degwhere = 135;
-         }else if(nir1 = 8){
-          degwhere = 157.5;
-         }
-      }else{     
-         if(nir2 = 1){
-          degwhere = 180;
-         }else if(nir2 = 2){
-          degwhere = 202.5;
-         }else if(nir2 = 3){
-          degwhere = 225;
-         }else if(nir2 = 4){
-          degwhere = 247.5;
-         }else if(nir2 = 5){
-          degwhere = 270;
-         }else if(nir2 = 6){
-          degwhere = 292.5;
-         }else if(nir2 = 7){
-          degwhere = 315; 
-         }else if(nir2 = 8){
-          degwhere = 337.5; 
-         }
-      }
-      Serial.print(degwhere);
-      Serial.println(" degrees.");
-      drive(75, degwhere);
-      delay(1);
-    }
-  }else{
-    Serial.println("softPWR is off, waiting");
-    delay(100);  
-  }
+  drive(255, 0);
+  Serial.println("ffor");
+  delay(1500);
+  drive(255, 180);
+  Serial.println("back");
+  delay(1500);
+
 }
---
+++ CODE/debug/driving/onemotor_softPWM/onemotor_softPWM.ino
@@ -0,0 +1,126 @@
+#include <TimerThree.h>
+
+int enA = 1;  //PWM pin
+int enB = 2;  //GND
+int pwm1 = 3; //
+int pwm2 = 4;
+int encoA = 5;
+int encoB = 6;
+
+int speed = 1000; // speed 0-1
+
+double sp = 0.00;
+double rpm = 0.00;
+volatile long encount = 0;
+unsigned long Ltime = 0;
+unsigned long Ctime = 0;
+unsigned long Ptime = 0;
+
+volatile int PWMval = 0;   // PWM value 0-100
+
+
+void setup() {
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(pwm1, OUTPUT);
+  pinMode(pwm2, OUTPUT);
+
+  pinMode(encoA, INPUT_PULLUP);
+  pinMode(encoB, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(encoA), encoderISR, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encoB), encoderISR, CHANGE);
+
+  Timer3.initialize(800); // 800 microseconds or 1.25kHz
+  Timer3.attachInterrupt(updatePWM); 
+
+  Serial.begin(9600);
+}
+
+void updatePWM() {
+  static int counter = 0;
+
+  if (counter < PWMval) {
+    digitalWrite(enA, HIGH);
+  } else {
+    digitalWrite(enA, LOW);
+  }
+
+  counter++;
+  if (counter >= 100) {
+    counter = 0;
+  }
+}
+void mcpsoftpwm(int value) {
+  if (value < 0) value = 0;
+  if (value > 100) value = 100;
+
+  Serial.print("softPWM pin: enA, value: ");
+  Serial.println(value); 
+  PWMval = value; 
+}
+
+void encoderISR() {
+  encount++;
+}
+
+void readenc() {
+  Ctime = micros();
+  Ptime = Ctime - Ltime;
+  Ltime = Ctime;
+  rpm = (encount / 211.2) * (60000000.0 / Ptime);
+  Serial.print("RPM: ");
+  Serial.print(rpm);
+  Serial.print("    Sample period:");
+  Serial.print(Ptime);
+  Serial.print("ms, encoder count:");
+  Serial.println(encount);
+
+  encount = 0;
+}
+void resetenc(){
+  Ctime = micros();
+  Ltime = Ctime;
+  encount = 0;
+}
+
+void move(int sped) {
+  sp = abs(sped)*0.1;
+  mcpsoftpwm(sp);
+  digitalWrite(enB, LOW);
+  if (sped < 0) {
+    Serial.print("back ");
+    digitalWrite(pwm1, LOW);
+    digitalWrite(pwm2, HIGH);
+  } else {
+    Serial.print("forward ");
+    digitalWrite(pwm1, HIGH);
+    digitalWrite(pwm2, LOW);
+  } 
+  Serial.println(sp);
+}
+
+void loop() {
+  move(speed);
+  delay(1000);
+  readenc();
+  delay(1000);
+
+  Serial.println();
+
+  move(0);
+  delay(1000);
+  resetenc();
+  delay(1000);
+  readenc();
+  delay(1000);
+
+  Serial.println();
+
+  move(500);
+  delay(1000);
+  readenc();
+  delay(1000);
+
+  Serial.println();
+}
